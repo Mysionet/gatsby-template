847b219068138650e1fcd2ad8c2ae37e090d5030
Kacper Gajda <lorenstenlelak@gmail.com>
2023-10-19 23:47:59 +0200

    files structure

---
+++ src/components/Component.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Component = () => {
+	return (
+		<div>
+			<p>component</p>
+		</div>
+	)
+}
+
+export default Component
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import {ThemeProvider} from 'styled-components'
 import theme from '../assets/theme'
 import GlobalStyle from '../assets/globalStyle'
+import Component from '../components/Component'
 
 const IndexPage = () => {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
 			<h1>template</h1>
+			<Component />
 		</ThemeProvider>
 	)
 }
